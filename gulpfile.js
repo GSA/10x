c0b307f02ea23189c11ed01374f4ce065944f043
Jeremy Zilar <jeremyzilar@gmail.com>
2019-08-19 15:02:15 -0400

    adding 'build-app' to the gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -102,9 +102,9 @@ gulp.task('build-app', gulp.series('build-sass'), function() {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-gulp.task('watch', gulp.series('clean-css','build-sass'), function (){
-  gulp.watch(`${PROJECT_SASS_SRC}/*.scss`, gulp.series('clean-css','build-sass'))
-  gulp.watch(`${USWDS_SRC}/stylesheets/*.scss`, gulp.series('clean-css', 'build-sass'))
+gulp.task('watch', gulp.series('clean-css','build-sass', 'build-app'), function (){
+  gulp.watch(`${PROJECT_SASS_SRC}/*.scss`, gulp.series('clean-css','build-sass', 'build-app'))
+  gulp.watch(`${USWDS_SRC}/stylesheets/*.scss`, gulp.series('clean-css', 'build-sass', 'build-app'))
 });
 
 
