f6e5459eff54abae6cce740d83982eb893de4f09
Tim Plummer <tplummer@flexion.us>
2020-10-07 14:59:21 -0500

    Update ProjectCard.js

--- src/features/ProjectList/ProjectCard.js
+++ src/features/ProjectList/ProjectCard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Card from "components/Card";
 import ProjectStatus from "./ProjectStatus";
 import Button from "components/Button";
+import Mdx from "features/Mdx";
 
 const ProjectCard = ({ data }) => {
   return (
@@ -18,11 +19,13 @@ const ProjectCard = ({ data }) => {
         meta={`Type: ${data.projectType}`}
         footer={<ProjectStatus data={data.phaseData} />}
       >
-        <div className="ProjectCard__excerpt">{data.excerpt}</div>
+        <div className="ProjectCard__excerpt">
+          <Mdx>{data.excerpt}</Mdx>
+        </div>
         {data.topics && (
           <div className="ProjectCard__topics">
             <span className="ProjectCard__topics-heading">
-              Topics & Audience
+              IntendedAudience
             </span>
             {data.topics.split(",").map((item, i) => (
               <span key={`topic-item-${i}`} className="ProjectCard__tag">
