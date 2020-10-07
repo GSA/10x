ff0707a5d20c7962ffc88e9584f0d294761d7dce
Tim Plummer <tplummer@flexion.us>
2020-10-07 18:29:33 -0500

    update project card link

--- src/features/ProjectList/ProjectCard.js
+++ src/features/ProjectList/ProjectCard.js
@@ -5,42 +5,46 @@ import ProjectStatus from "./ProjectStatus";
 import Button from "components/Button";
 import Mdx from "features/Mdx";
 
-const ProjectCard = ({ data }) => {
-  return (
-    <Button
-      variant="link"
-      url={data.projectLink || "/our-projects"}
-      className="ProjectCard__link"
-    >
-      <Card
-        className="ProjectCard"
-        title={data.title}
-        subtitle={data.subtitle}
-        meta={`Type: ${data.projectType}`}
-        footer={<ProjectStatus data={data.phaseData} />}
-      >
-        <div className="ProjectCard__excerpt">
-          <Mdx>{data.excerpt}</Mdx>
-        </div>
-        {data.topics && (
-          <div className="ProjectCard__topics">
-            <span className="ProjectCard__topics-heading">
-              IntendedAudience
-            </span>
-            {data.topics.split(",").map((item, i) => (
-              <span key={`topic-item-${i}`} className="ProjectCard__tag">
-                {item}
-              </span>
-            ))}
-          </div>
-        )}
-      </Card>
-    </Button>
-  );
-};
+const ProjectCard = ({ data }) => (
+  <Card
+    className="ProjectCard"
+    title={data.title}
+    subtitle={data.subtitle}
+    meta={`Type: ${data.projectType}`}
+    footer={<ProjectStatus data={data.phaseData} />}
+  >
+    <div className="ProjectCard__excerpt">
+      <Mdx>{data.excerpt}</Mdx>
+    </div>
+    {data.topics && (
+      <div className="ProjectCard__topics">
+        <span className="ProjectCard__topics-heading">IntendedAudience</span>
+        {data.topics.split(",").map((item, i) => (
+          <span key={`topic-item-${i}`} className="ProjectCard__tag">
+            {item}
+          </span>
+        ))}
+      </div>
+    )}
+  </Card>
+);
 
 ProjectCard.propTypes = {
   data: PropTypes.object,
 };
 
-export default ProjectCard;
+export default (props) => {
+  if (!props.data.projectLink) {
+    return <ProjectCard {...props} />;
+  }
+
+  return (
+    <Button
+      variant="link"
+      url={props.data.projectLink}
+      className="ProjectCard__link"
+    >
+      <ProjectCard {...props} />
+    </Button>
+  );
+};
