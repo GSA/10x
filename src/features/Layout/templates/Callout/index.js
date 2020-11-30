b3fad122bfbae01d356d8bf825884c20a88ed2e9
Tim Plummer <tplummer@flexion.us>
2020-11-29 19:49:36 -0600

    update config

---
+++ src/features/Layout/templates/Callout/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const index = ({ className, title, text }) => {
+  return <div></div>;
+};
+
+index.propTypes = {};
+
+export default index;
