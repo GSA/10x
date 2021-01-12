09066a4515c706c7723f01761e2327bd4c8dfba0
Tim Plummer <tplummer@flexion.us>
2021-01-11 21:09:49 -0600

    other style changes

--- src/app/MenuModule/context/index.js
+++ src/app/MenuModule/context/index.js
@@ -7,5 +7,8 @@ let context = test;
 if (process.env.NODE_ENV !== "test") {
   context = ssr;
 }
+if (process.env.REACT_APP_STATE_MODE === "ssr") {
+  context = ssr;
+}
 
 export default context;
--- src/app/SettingsModule/context/index.js
+++ src/app/SettingsModule/context/index.js
@@ -1,11 +1,16 @@
 /* istanbul ignore file */
 import * as settingsAPI from "./settingsAPI";
+import * as ssr from "./ssr";
 import * as test from "./test";
 
 let context = test;
 
 if (process.env.NODE_ENV !== "test") {
   context = settingsAPI;
+}
+
+if (process.env.REACT_APP_STATE_MODE === "ssr") {
+  context = ssr;
 }
 
 export default context;
