82b448f20bb7448fbd53c703f0accf89e9600459
Sarah Rudder <sarah.rudder@gsa.gov>
2021-04-02 15:37:21 -0500

    remove loading on PostList

--- src/features/Layout/templates/PostList/index.js
+++ src/features/Layout/templates/PostList/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
-import classnames from "classnames";
 import ContentList from "features/ContentList";
 import PostCard from "./PostCard";
 import { Grid, Row } from "components/Grid";
 
 const PostList = () => {
-  const loading = useSelector((state) => state.content.list.pending);
   return (
     <Grid>
-      <Row gap="4" className={classnames({ isLoading: loading }), "PostList TxCards TxShadowedCard"}>
+      <Row gap="4" className="PostList TxCards TxShadowedCard">
         <ContentList type="post" render={PostCard} />
       </Row>
     </Grid>
