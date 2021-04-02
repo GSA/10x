00586664fe42f9e80a9f013fa09f857477374454
Sarah Rudder <sarah.rudder@gsa.gov>
2021-04-02 12:55:04 -0500

    remove redundant empty object decl

--- src/features/Layout/index.js
+++ src/features/Layout/index.js
@@ -37,7 +37,7 @@ const components = {
       <Mdx>{body}</Mdx>
     </div>
   ),
-  phaseStatus: ({ data: { phaseData } = {} = {} }) => (
+  phaseStatus: ({ data: { phaseData } }) => (
     <PhaseStatus data={phaseData} />
   ),
   phaseDescription: PhaseDescription,
