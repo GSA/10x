fa4c08f8e250148b2660e2cbc4c5eb204c7e1c51
Sarah Rudder <sarah.rudder@gsa.gov>
2021-04-26 20:09:48 -0500

    make sure to close mobile menu after navigation

--- src/templates/Primary/Header.js
+++ src/templates/Primary/Header.js
@@ -47,6 +47,7 @@ const Header = ({ logo, className, variant }) => {
   const handleClick = (link) => {
     setActiveMenuItem(null);
     history.push(`${link}`);
+    handleMenuToggle(false);
   };
 
   useOnPathChange(() => {
