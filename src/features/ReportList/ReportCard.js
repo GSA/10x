e3b28533314ae85c1443ae10f2ba64a4b823a64d
mitchmoccia <code@3dx8.com>
2022-04-06 21:03:12 -0400

    Rework the Report Cards, add a large year header for each card and remove duplicate sub head

--- src/features/ReportList/ReportCard.js
+++ src/features/ReportList/ReportCard.js
@@ -1,19 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
-import Card from "components/Card";
-import Link from "features/Link";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Card from 'components/Card';
+import Link from 'features/Link';
 
 const ReportCard = ({ data }) => {
   /* istanbul ignore next */
-  const slug = data.reportUrl || (data.path) + "/";
+  const slug = data.reportUrl || data.path + '/';
 
   return (
     <div
       className={classnames({
-        "grid-col-12": true,
-        "desktop:grid-col-6": true
-      })}>
+        'grid-col-12': true,
+        'desktop:grid-col-6': true,
+      })}
+    >
       <Link url={slug} className="ReportCard__link" target={data.reportUrl ? '_blank' : '_top'}>
         <Card
           className={classnames({
@@ -21,13 +22,21 @@ const ReportCard = ({ data }) => {
             [`template-${data.template}`]: true,
           })}
         >
+          <h1
+            className={classnames({
+              'usa-card__heading': true,
+              'pdf-icon': data.pdf,
+            })}
+          >
+            {data.year || data.year}
+          </h1>
           <h2
             className={classnames({
-              "usa-card__heading": true,
-              "pdf-icon": data.pdf,
+              'usa-card__subhead': true,
             })}
-          >{data.subtitle || data.subtitle}</h2>
-          <h3 className="usa-card__subhead">{data.title || data.title}</h3>
+          >
+            {data.title || data.title}
+          </h2>
           <div className="ReportCard__excerpt">{data.excerpt || data.intro}</div>
         </Card>
       </Link>
