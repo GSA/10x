bd4117bf3050472408b693a427897782934a2583
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-12-08 17:38:04 -0600

    linting issues

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,43 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Default from "./Default";
-import Page from "./Page";
-import Post from "./Post";
-import Project from "./Project";
-import Report from "./Report";
+import { Switch, Route } from "react-router-dom"
+import Default from "./Default"
+import Page from "./Page"
+import Post from "./Post"
+import Project from "./Project"
+import Report from "./Report"
 
-export default ({ location }) => (
+const Routes = () => (
   <Switch>
-    <Route key="default" path="/" exact>
+    <Route
+      key="default"
+      path="/"
+      exact
+    >
       <Default />
     </Route>
-    <Route key="project" path="/projects/:name">
+    <Route
+      key="project"
+      path="/projects/:name"
+    >
       <Project />
     </Route>
-    <Route key="post" path="/posts/:name">
+    <Route
+      key="post"
+      path="/posts/:name"
+    >
       <Post />
     </Route>
-    <Route key="report" path="/reports/:name">
+    <Route
+      key="report"
+      path="/reports/:name"
+    >
       <Report />
     </Route>
-    <Route key="page" path="/:name">
+    <Route
+      key="page"
+      path="/:name"
+    >
       <Page />
     </Route>
   </Switch>
-);
+)
+export default Routes
