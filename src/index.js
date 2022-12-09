11f42ece2ab678efefb650ef7a0d75c16cf82460
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-12-09 13:55:04 -0600

    attempts routing fix

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import React from "react";
-import ReactDOM, { hydrate, render } from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./routes";
-import store from "./app";
-import * as serviceWorker from "./serviceWorker";
-import Primary from "templates/Primary";
-import registerFontAwesome from "utils/registerFontAwesome";
-import "styles/index.scss";
+import React from "react"
+import ReactDOM, { hydrate, render } from "react-dom"
+import { Provider } from "react-redux"
+import { BrowserRouter as Router } from "react-router-dom"
+import Routes from "./routes"
+import store from "./app"
+import * as serviceWorker from "./serviceWorker"
+import Primary from "templates/Primary"
+import registerFontAwesome from "utils/registerFontAwesome"
+import "styles/index.scss"
 
-registerFontAwesome();
+registerFontAwesome()
 
 if (process.env.NODE_ENV !== "production") {
-  const axe = require("react-axe");
-  axe(React, ReactDOM, 1000);
+  const axe = require("react-axe")
+  axe(React, ReactDOM, 1000)
 }
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <Primary>
         <Routes />
       </Primary>
     </Provider>
   </Router>
-);
+)
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")
 if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
+  hydrate(<App />, rootElement)
 } else {
-  render(<App />, rootElement);
+  render(<App />, rootElement)
 }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
